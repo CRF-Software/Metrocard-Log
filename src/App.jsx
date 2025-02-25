305e830fb155aa416dc6dc66683e7a126edf3a5f
CRF-Software <duhartd@icahnhouse.org>
2025-02-25 14:26:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 // Configure Amplify with your AWS settings
